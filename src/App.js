ac884791733cc73ef07e49b1a02faf8faf50368e
sunandbean <sunbin.vincent.kim@gmail.com>
2022-04-09 10:41:52 +0900

    deps in useEffect

--- src/App.js
+++ src/App.js
@@ -2,14 +2,29 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
   console.log("I run all the time");
-  const iRunOnlyOnce = () => {
+  useEffect(() => {
+    console.log("Update counter ", counter);
+  }, [counter]);
+  useEffect(() => {
     console.log("I run only once");
-  };
-  useEffect(iRunOnlyOnce, []);
+  }, []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("SEARCH FOR", keyword);
+    }
+  }, [keyword]);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>Click me</button>
     </div>
