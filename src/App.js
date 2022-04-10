797f6046f589b8406e8fbeeb5f2fa1d6548dc533
sunandbean <sunbin.vincent.kim@gmail.com>
2022-04-10 13:26:54 +0900

    finish router without parameters (static url)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
 import Home from "./routes/Home";
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Route path="/hello">
+          <h1>Hello!</h1>
+        </Route>
+        <Route path="/movie">
+          <Detail />
+        </Route>
         <Route path="/">
           <Home />
         </Route>
