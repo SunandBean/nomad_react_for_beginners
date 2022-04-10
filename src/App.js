680b97a95c98158baebbe17eed94974a8c5bb118
sunandbean <sunbin.vincent.kim@gmail.com>
2022-04-10 10:41:46 +0900

    change list to select

--- src/App.js
+++ src/App.js
@@ -15,13 +15,13 @@ function App() {
     <div>
       <h1>The Coins! ({coins.length})</h1>
       {loading ? <strong>Loading... </strong> : null}
-      <ul>
+      <select>
         {coins.map((coin) => (
-          <li key={coin.id}>
+          <option key={coin.id}>
             {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
-          </li>
+          </option>
         ))}
-      </ul>
+      </select>
     </div>
   );
 }
